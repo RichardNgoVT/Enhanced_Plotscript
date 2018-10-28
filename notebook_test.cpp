2772a32117b39192fe586c4564fb6552f3260501
Richard Ngo <R1ch4rd@vt.edu>
2018-10-28 03:19:43 -0400

    added point tests

--- notebook_test.cpp
+++ notebook_test.cpp
@@ -8,6 +8,7 @@ private slots:
   void initTestCase();
   void defaultTest();
   void plotscriptTests();
+  void pointTests();
 
   // TODO: implement additional tests here
   
@@ -26,11 +27,22 @@ void NotebookTest::defaultTest() {
 	QVERIFY2(outwid, "Could not find out widget with name: 'output'");
 }
 
+
+/* Check location
+*/
 void NotebookTest::plotscriptTests()
 {
 	//testing (get-property "key" (3))
 	auto inwid = widget.findChild<InputWidget *>("input");
 	auto outwid = widget.findChild<OutputWidget *>("output");
+
+	//clears just in case
+	QTest::mouseClick(inwid, Qt::LeftButton, Qt::NoModifier, QPoint(0, 0), 10);
+	QTest::keyPress(inwid, Qt::Key_A, Qt::ControlModifier, 10);
+	QTest::keyPress(inwid, Qt::Key_A, Qt::ControlModifier, 10);
+	QTest::keyPress(inwid, Qt::Key_Backspace, Qt::NoModifier, 10);
+	QTest::keyRelease(inwid, Qt::Key_Backspace, Qt::NoModifier, 10);
+
 	QTest::mouseClick(inwid, Qt::LeftButton, Qt::NoModifier, QPoint(0, 0), 10);
 	QTest::keyClicks(inwid, "(get-property \"key\" (3))");
 	QTest::keyPress(inwid, Qt::Key_Return, Qt::ShiftModifier, 10);
@@ -116,11 +128,102 @@ void NotebookTest::plotscriptTests()
 	QVERIFY2(viewwid->items().size() == 1, "incorrect number of items in QGraphicsView");
 	text = qgraphicsitem_cast<QGraphicsTextItem *>(viewwid->items()[0]);
 	QCOMPARE(text->toPlainText(), QString("Error in call to first, argument not a list\n"));
-
-	
-
 }
 
+void NotebookTest::pointTests()
+{
+	//testing (make-point 0 0)
+	auto inwid = widget.findChild<InputWidget *>("input");
+	auto outwid = widget.findChild<OutputWidget *>("output");
 
+	//clears just in case
+	QTest::mouseClick(inwid, Qt::LeftButton, Qt::NoModifier, QPoint(0, 0), 10);
+	QTest::keyPress(inwid, Qt::Key_A, Qt::ControlModifier, 10);
+	QTest::keyPress(inwid, Qt::Key_A, Qt::ControlModifier, 10);
+	QTest::keyPress(inwid, Qt::Key_Backspace, Qt::NoModifier, 10);
+	QTest::keyRelease(inwid, Qt::Key_Backspace, Qt::NoModifier, 10);
+
+	QTest::mouseClick(inwid, Qt::LeftButton, Qt::NoModifier, QPoint(0, 0), 10);
+	QTest::keyClicks(inwid, "(make-point 0 0)");
+	QTest::keyPress(inwid, Qt::Key_Return, Qt::ShiftModifier, 10);
+	QTest::keyRelease(inwid, Qt::Key_Return, Qt::ShiftModifier, 10);
+	auto viewwid = outwid->findChild<QGraphicsView *>();
+	QVERIFY2(viewwid, "Could not find view widget");
+	QVERIFY2(viewwid->items().size() == 1, "incorrect number of items in QGraphicsView");
+	QGraphicsEllipseItem * point = qgraphicsitem_cast<QGraphicsEllipseItem *>(viewwid->items()[0]);
+	QCOMPARE(point->brush(), Qt::SolidPattern);//check if filled in
+	QRectF pos;
+	double size = 0.0;
+	pos.setRect(0.0-(size/2.0), 0.0-(size/2.0), size, size);
+	QCOMPARE(point->rect(), pos);
+
+	//(set-property "size" 20 (make-point 0 0))
+	QTest::mouseClick(inwid, Qt::LeftButton, Qt::NoModifier, QPoint(0, 0), 10);
+	QTest::keyPress(inwid, Qt::Key_A, Qt::ControlModifier, 10);
+	QTest::keyPress(inwid, Qt::Key_A, Qt::ControlModifier, 10);
+	QTest::keyPress(inwid, Qt::Key_Backspace, Qt::NoModifier, 10);
+	QTest::keyRelease(inwid, Qt::Key_Backspace, Qt::NoModifier, 10);
+	QTest::keyClicks(inwid, "(set-property \"size\" 20 (make-point 0 0))");
+	QTest::keyPress(inwid, Qt::Key_Return, Qt::ShiftModifier, 10);
+	QTest::keyRelease(inwid, Qt::Key_Return, Qt::ShiftModifier, 10);
+	QVERIFY2(viewwid->items().size() == 1, "incorrect number of items in QGraphicsView");
+	point = qgraphicsitem_cast<QGraphicsEllipseItem *>(viewwid->items()[0]);
+	QCOMPARE(point->brush(), Qt::SolidPattern);//check if filled in
+	size = 20;
+	pos.setRect(0.0 - (size / 2.0), 0.0 - (size / 2.0), size, size);
+	QCOMPARE(point->rect(), pos);
+
+	//(list (set-property "size" 1 (make-point 0 0)) (set-property "size" 2 (make-point 4 0)) (set-property "size" 4 (make-point 8 0)) (set-property "size" 8 (make-point 16 0)) (set-property "size" 16 (make-point 32 0)) (set-property "size" 32 (make-point 64 0)))
+	QTest::mouseClick(inwid, Qt::LeftButton, Qt::NoModifier, QPoint(0, 0), 10);
+	QTest::keyPress(inwid, Qt::Key_A, Qt::ControlModifier, 10);
+	QTest::keyPress(inwid, Qt::Key_A, Qt::ControlModifier, 10);
+	QTest::keyPress(inwid, Qt::Key_Backspace, Qt::NoModifier, 10);
+	QTest::keyRelease(inwid, Qt::Key_Backspace, Qt::NoModifier, 10);
+	QTest::keyClicks(inwid, "(list (set-property \"size\" 1 (make-point 0 0)) (set-property \"size\" 2 (make-point 4 0)) (set-property \"size\" 4 (make-point 8 0)) (set-property \"size\" 8 (make-point 16 0)) (set-property \"size\" 16 (make-point 32 0)) (set-property \"size\" 32 (make-point 64 0)))");
+	QTest::keyPress(inwid, Qt::Key_Return, Qt::ShiftModifier, 10);
+	QTest::keyRelease(inwid, Qt::Key_Return, Qt::ShiftModifier, 10);
+	QVERIFY2(viewwid->items().size() == 6, "incorrect number of items in QGraphicsView");
+	for (int i = 0; i < 6; i++)
+	{
+		point = qgraphicsitem_cast<QGraphicsEllipseItem *>(viewwid->items()[(5-i)]);
+		QCOMPARE(point->brush(), Qt::SolidPattern);//check if filled in
+		size = 1.0*(pow(2, i));
+		if (i == 0)//starting point at (0, 0)
+		{
+			pos.setRect(0.0 - (size / 2.0), 0.0 - (size / 2.0), size, size);
+		}
+		else
+		{
+			pos.setRect(1.0*pow(2, i+1) - (size / 2.0), 0.0 - (size / 2.0), size, size);
+		}
+		QCOMPARE(point->rect(), pos);
+	}
+	
+	//(list (set-property "size" 1 (make-point 0 0)) (set-property "size" 2 (make-point 0 4)) (set-property "size" 4 (make-point 0 8)) (set-property "size" 8 (make-point 0 16)) (set-property "size" 16 (make-point 0 32)) (set-property "size" 32 (make-point 0 64)))
+	QTest::mouseClick(inwid, Qt::LeftButton, Qt::NoModifier, QPoint(0, 0), 10);
+	QTest::keyPress(inwid, Qt::Key_A, Qt::ControlModifier, 10);
+	QTest::keyPress(inwid, Qt::Key_A, Qt::ControlModifier, 10);
+	QTest::keyPress(inwid, Qt::Key_Backspace, Qt::NoModifier, 10);
+	QTest::keyRelease(inwid, Qt::Key_Backspace, Qt::NoModifier, 10);
+	QTest::keyClicks(inwid, "(list (set-property \"size\" 1 (make-point 0 0)) (set-property \"size\" 2 (make-point 0 4)) (set-property \"size\" 4 (make-point 0 8)) (set-property \"size\" 8 (make-point 0 16)) (set-property \"size\" 16 (make-point 0 32)) (set-property \"size\" 32 (make-point 0 64)))");
+	QTest::keyPress(inwid, Qt::Key_Return, Qt::ShiftModifier, 10);
+	QTest::keyRelease(inwid, Qt::Key_Return, Qt::ShiftModifier, 10);
+	QVERIFY2(viewwid->items().size() == 6, "incorrect number of items in QGraphicsView");
+	for (int i = 0; i < 6; i++)
+	{
+		point = qgraphicsitem_cast<QGraphicsEllipseItem *>(viewwid->items()[(5 - i)]);
+		QCOMPARE(point->brush(), Qt::SolidPattern);//check if filled in
+		size = 1.0*(pow(2, i));
+		if (i == 0)//starting point at (0, 0)
+		{
+			pos.setRect(0.0 - (size / 2.0), 0.0 - (size / 2.0), size, size);
+		}
+		else
+		{
+			pos.setRect(0.0 - (size / 2.0), 1.0*pow(2, i + 1) - (size / 2.0), size, size);
+		}
+		QCOMPARE(point->rect(), pos);
+	}
+}
 QTEST_MAIN(NotebookTest)
 #include "notebook_test.moc"
